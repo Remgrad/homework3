f95b8b2fa626803ff3db5cb5366f3c72846eeae4
Remgrad <remgrad622@gmail.com>
2024-12-01 02:01:50 +0300

    Произвольное число параметров

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+#Объявите функцию single_root_words и напишите в ней параметры root_word и *other_words.
+def single_root_words (root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+    for word in other_words:
+        other_words_lower = word.lower()
+        if root_word_lower.count(other_words_lower) or other_words_lower.count(root_word_lower):
+            same_words.append(word)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
